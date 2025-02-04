7804ae0726d0d1bd685f94116851550c7c70a4e5
Blade-iii <54221403+Blade-iii@users.noreply.github.com>
2025-02-04 15:20:16 +0000

    Added the ability to create a note
    Notes page added

---
+++ Notes/NotesPage.xaml.cs
@@ -0,0 +1,29 @@
+using System.Runtime.CompilerServices;
+
+namespace Notes;
+
+public partial class NotesPage : ContentPage
+{ 
+			string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt") ;
+	public NotesPage()
+	{
+		InitializeComponent();
+
+		if (File.Exists(_fileName)){
+			TextEditor.Text = File.ReadAllText(_fileName);
+		}
+	}
+
+	private void SaveButton_Clicked(object sender,EventArgs e){
+			// Save the file
+			File.WriteAllText(_fileName,TextEditor.Text);
+		}
+	private void DeleteButton_Clicked(object sender,EventArgs e){
+		// Delete the file
+		if (File.Exists(_fileName)){
+			File.Delete(_fileName);
+			// Empty the text field
+			TextEditor.Text = string.Empty;
+		}
+	}
+}
